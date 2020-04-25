556ad6ed532a0cf09fe97fadc22b94469f6e1eff
michele-savino <michele-pio.savino@studenti.isae-supaero.fr>
2020-04-26 00:49:04 +0200

    Research form added

--- music_website/disks/urls.py
+++ music_website/disks/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.album_list, name='album_list'),
     path('album/<int:id>', views.album_show, name='album'),
+    path('search', views.research, name='research')
 ]
--- music_website/disks/views.py
+++ music_website/disks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
-from disks.models import Album
+from disks.models import Album, Artist, Track
+from disks.forms import SearchForm
 
 
 def album_list(request):
@@ -14,3 +15,15 @@ def album_show(request, id):
     tracks = album.track_set.all()
 
     return render(request, 'disks/album.html', locals())
+
+
+def research(request):
+    form = SearchForm(request.GET)
+    if form.is_valid():
+        query = form.cleaned_data['query']
+        what = form.cleaned_data['what']
+        albums = Album.objects.filter(Title__contains=query)
+        tracks = Track.objects.filter(Name__contains=query)
+        artists = Artist.objects.filter(Name__contains=query)
+
+    return render(request, 'disks/search_results.html', locals(), {'form': form})
