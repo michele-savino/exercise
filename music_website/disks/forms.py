3721f965b9e5a90971f92d1fa17e5c1a078207bc
michele-savino <michele-pio.savino@studenti.isae-supaero.fr>
2020-04-26 17:19:52 +0200

    Last commit

--- music_website/disks/forms.py
+++ music_website/disks/forms.py
@@ -1,13 +1,13 @@
 from django import forms
-from .models import Album
+from .models import Album, Artist
 
 
 class SearchForm(forms.Form):
-    query = forms.CharField(label="Search", max_length=220, widget=forms.TextInput())
-    what = forms.CharField(widget=forms.TextInput())
+    query = forms.CharField(label="", max_length=220, widget=forms.TextInput())
+    what = forms.CharField(label="", widget=forms.TextInput())
 
 
 class NewAlbumForm(forms.ModelForm):
     class Meta:
         model = Album
-        fields = ('Title', 'artist')
+        fields = '__all__'
--- music_website/disks/models.py
+++ music_website/disks/models.py
@@ -1,28 +1,39 @@
+from math import ceil
+
 from django.db import models
 
 
-class Track(models.Model):
-    album = models.ForeignKey('Album', on_delete=models.CASCADE)
-    Name = models.CharField(max_length=200)
-    Composer = models.CharField(max_length=220)
-    Milliseconds = models.TextField()
-    Bytes = models.IntegerField()
-    UnitPrice = models.DecimalField(max_digits=10, decimal_places=2)
+class Artist(models.Model):
+    Name = models.CharField(max_length=120)
+
+    class Meta:
+        db_table = "disks_artist"
 
     def __str__(self):
         return self.Name
 
 
 class Album(models.Model):
-    artist = models.ForeignKey('Artist', on_delete=models.CASCADE)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     Title = models.CharField(max_length=160)
+
+    class Meta:
+        db_table = "disks_album"
 
     def __str__(self):
         return self.Title
 
 
-class Artist(models.Model):
-    Name = models.CharField(max_length=120)
+class Track(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    Name = models.CharField(max_length=200)
+    Composer = models.CharField(max_length=220)
+    Milliseconds = models.TextField()
+    Bytes = models.IntegerField()
+    UnitPrice = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = "disks_track"
 
     def __str__(self):
         return self.Name
